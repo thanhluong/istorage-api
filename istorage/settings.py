f654673599fd51c74d664a544e9e8658f0009599
unknown <nguyenxuanhieuktb10@gmail.com>
2023-04-05 11:57:05 +0700

    add cors

--- istorage/settings.py
+++ istorage/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-ft^^8vx@n(z=5%8-70p51beg=!=*!u^$ie4)^hkar@7a98=gf$
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -39,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'file_storage'
+    'file_storage',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'istorage.urls'
