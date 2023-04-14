66e6411a5a66c83bd77ddaf6f380c56f555c2a57
Hai Nguyen <huyhai1803@gmail.com>
2023-04-14 22:22:05 +0700

    Add prefix api/ to url of admin

--- istorage/urls.py
+++ istorage/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('file_storage.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
