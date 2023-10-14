3a25018ca7f51a5c8d626bb526f9f4ec970cb7ba
thanhluong <doannguyenthanhluong@gmail.com>
2023-10-15 02:33:47 +0700

    Add SiteMenu to Admin

--- file_storage/admin.py
+++ file_storage/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Document, GovFile, GovFileProfile
+from .models import SiteMenu
 
 
 class DocumentAdmin(admin.ModelAdmin):
@@ -13,3 +14,4 @@ class GovFileAdmin(admin.ModelAdmin):
 admin.site.register(Document)
 admin.site.register(GovFile)
 admin.site.register(GovFileProfile)
+admin.site.register(SiteMenu)
