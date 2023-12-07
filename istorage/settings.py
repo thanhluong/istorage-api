a1bd76eb75753b5ca64571f0a2e0d0fd622d3bd4
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-08 00:04:56 +0700

    Add DEBUG to settings.py

--- istorage/settings.py
+++ istorage/settings.py
@@ -16,6 +16,7 @@ from . import local_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
