ac72710e9fb768b18729e4c43ab9d907e4690504
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-11 18:57:19 +0700

    Add APIs

--- file_storage/urls.py
+++ file_storage/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from .views.document import DocumentUploadView, GetDocumentByGovFileId, DeleteDocumentById, UpdateDocumentById
 from .views.gov_file import GetGovFiles, CreateGovFile, UpdateGovFileById, UpdateGovFileStateById, DeleteGovFileById
 from .views.search import FullTextSearchView
@@ -19,7 +24,26 @@ from .views.storage_unit import WarehouseRoomListView, WarehouseRoomDetailView
 from .views.storage_unit import ShelfListView, ShelfDetailView
 from .views.storage_unit import DrawerListView, DrawerDetailView
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny, permissions.IsAuthenticated],
+)
+
+
 urlpatterns = [
+    # Swagger views
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
     # User APIs,
     path('user', StorageUserListApiView.as_view(), name='user'),
     path('user/user_id/<int:user_id>', StorageUserDetailApiView.as_view(), name='user_detail'),
--- istorage/settings.py
+++ istorage/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'file_storage',
     'corsheaders',
 ]
