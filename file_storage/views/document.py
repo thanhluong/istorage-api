9affdf2f8769ae8e1297958fbc08fdf963784d58
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-15 22:50:31 +0700

    Print debug info

--- file_storage/views/document.py
+++ file_storage/views/document.py
@@ -116,8 +116,8 @@ class GetDocumentByGovFileId(APIView):
             serialization_result = serializer.data
             result = []
             for doc in serialization_result:
-                doc['url'] = "https://" + request.get_host() + '/api/display_pdf'
-                              # + '/' + doc['gov_file_id'] + '/' + doc['id']
+                doc['url'] = "https://" + request.get_host() + '/api/display_pdf' \
+                              + '/' + doc['gov_file_id'] + '/' + doc['id']
 
                 result.append(doc)
 
