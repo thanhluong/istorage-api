9b3a9d46104b6256482a40eba30ca918729b0ea1
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-15 13:59:17 +0700

    Authorize for all APIs

--- file_storage/urls.py
+++ file_storage/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
-   permission_classes=[permissions.IsAuthenticated, permissions.IsAuthenticated],
+   permission_classes=[permissions.AllowAny, permissions.IsAuthenticated],
 )
 
 
